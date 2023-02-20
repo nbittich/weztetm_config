ba9fb3e5b46f4d6824693c65cdcfc6be765ad538
Nordine Bittich <nordine@bittich.be>
2023-02-20 11:48:48 +0100

    autocommit

--- wezterm.lua
+++ wezterm.lua
@@ -1,26 +1,35 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  window_padding = {
-    left = '0.3cell',
-    right = '0cell',
-    top = '0.1cell',
-    bottom = '0cell',
-  },
-  keys = {
-    {
-      key = 'F11',
-      action = wezterm.action.ToggleFullScreen,
+    window_padding = {
+        left = "0.3cell",
+        right = "0cell",
+        top = "0.1cell",
+        bottom = "0cell",
     },
-    {
-      key = 't',
-      mods = 'CTRL',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+    keys = {
+        {
+            key = "F11",
+            action = wezterm.action.ToggleFullScreen,
+        },
+        {
+            key = "t",
+            mods = "CTRL",
+            action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+        },
+        {
+            key = "u",
+            mods = "CTRL",
+            action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+        },
+        {
+            key = "a",
+            mods = "CTRL",
+            action = wezterm.action.SplitPane({
+                direction = "Left",
+                command = { args = { "adana" } },
+                size = { Percent = 30 },
+            }),
+        },
     },
-    {
-      key = 'u',
-      mods = 'CTRL',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-    },
-  },
 }
